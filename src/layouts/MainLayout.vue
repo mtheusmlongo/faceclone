c0d6e38690ae1e51aad1837189cd4cf3223ed182
mtheus.longo <matheus.longo@gocaresaude.com.br>
2021-12-13 16:15:43 -0300

    atualizçao1

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,83 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="now items-center justify-between q-pt-sm">
+     
+      <q-img width="130px" class="q-mx-ms q-my-md" src= "../assets/facelogo.png" />
+      
+      <div>
+        <q-icon
+        class= "q-mx-sm q-pa-sm q-icon-custom"
+        name= "fas fa-search"
+        size= "25px"
+        color= "black"
+        />
+        <q-icon
+        class= "q-mx-sm q-pa-sm q-icon-custom"
+        name=" fab fa-facebook-messenger"
+        size= "25px"
+        color= "black"
+        /> 
+       </div>
+       <div class="flex flex-center full-whidth">
+         <q-tabs v-model="tab" class="text-teal q-mx-sm full-width">
+           <q-tab name="home" icon="fas fa-home" />
+           <q-tab nome="users" icon="fas fa-users"/>
+           <q-tab name="video" icon= "ondemand_video" />
+           <q-tab name="store" icon="notifications" />
+           <q-icon name="fas fa-bars" size="30px" class="q-mr-md q-icon-color" />
+           
+         </q-tabs>
+       </div>
+
+
+      </q-header>
+
+   
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+name: 'MainLayout',
+data () {
+  return {
+    tab : "home",
+  };
+
+ },
+};
+
+</script>
+
+<style lang="scss" scoped>
+.q-icon-custom {
+background-color: #f2f2f2;
+border-radius: 50%;
+}
+.q-tabs__content {
+  height: 70px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+} 
+.q-tab__icon {
+  font-size: 20px;
+
+}
+.q-tab {
+  padding:5px;
+  }
+  .q-tab--active{
+    color: #1678f3
+  }
+  .q-tab--inactive{
+     color:#656668
+  }
+
+</style>
+
---
+++ src/pages/Index.vue
@@ -0,0 +1,144 @@
+    <template>
+      <q-page class= "q-nt-sm">
+        <div class ="row q-pa-sm full-width">
+          <q-avatar class= "q-mr-sm">
+            <img src="https://cdn.quasar.dev/img/avatar.png" alt="" />
+          </q-avatar>
+          <q-input 
+          rounded
+          outlined 
+          v-model="text" 
+          label="No que você está pensando" 
+          label-color="black" 
+          >
+
+            </q-input>
+          </div>
+
+          <div class="line q-mt-md" />
+          <div class="row justify-around full-width q-pa-sm">
+            <div>
+              <q-icon
+              color="red"
+              name="fas fa-video"
+              size="18px"
+              class="q-mr-sm q-icon-color"
+              >
+              </q-icon>
+              <label>Ao Vivo</label>
+            </div>
+              <div class="vertical-line"> </div>
+              <div>
+                <q-icon
+                color="light-green-7"
+                name="far-fa-images"
+                size="20px"
+                class="q-mr-sm q-icon-color"
+                >
+                </q-icon>
+                
+                <label>F</label>
+                  <div class="vertical-line">
+                    
+                  </div>
+              </div>  
+            
+            <div>
+              <q-icon
+              color="purple"
+              name="meeting_room"
+              size="18px"
+              class="q-mr-sm q-icon-color"
+              >
+              </q-icon>
+              <label>Salas</label>
+            </div>
+          </div>
+              <div class="vertical-line"></div>
+          
+            
+          <div class="line-strong q-mt-md"></div>
+          
+          <q-scroll-area horizontal class="container-new-room full-whidth">
+          
+            <div   
+            class="row items-center justify-between q-px-md q-py-sm no-wrap full-width conta"
+            >
+            <div class="rounded-burron row text-center q-mr-md">
+              <div class ="row items-center justify-center">
+                <q-icon
+                name="fas fa-video"
+                color="purple"
+                size="28px"
+                class="q-mr-sm q-icon-color"
+                >
+                </q-icon>
+                
+              </div>
+            </div>
+            </div>
+          </q-scroll-area>
+            
+          
+      
+      
+      </q-page>
+      
+
+        
+        </template>    
+
+    <script>
+    export default {
+        name: "PageIndex",
+        data() {
+          return {
+            text :"",
+
+          };
+        },
+      };
+    </script>
+
+
+
+    <style lang="scss" scoped>
+           
+    .q-field {
+      width: 82%;
+    }
+    .q-field control {
+        height: 45px;
+      }
+    .conteiner-new-room{
+      background-color: #fff; 
+    }
+    .conteiner-button {
+      border:1px solid #cbed3c
+
+    }
+    .conteiner-new-room{
+      height: 70px;
+    }
+    .conteiner-room {
+      background-color:#fff ;
+    }
+    .rounded-button {
+      border:1px solid #cbced3 ;
+      border-radius: 27px ;
+      padding: 4px 10px;
+
+      div:first-child {
+        width: 50%;
+      }
+      div{
+        width: 50%;
+      }
+      label {
+        color: #161616;
+      }
+
+      
+
+    }
+    </style>
